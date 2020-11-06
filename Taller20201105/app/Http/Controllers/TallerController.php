9d0828f2d67f4515991d577143301d824abd1076
Vicente Aux <vauxrevelo@gmail.com>
2020-11-06 08:14:38 -0500

    Adición Taller Controller

---
+++ Taller20201105/app/Http/Controllers/TallerController.php
@@ -0,0 +1,9 @@
+
+<?php
+namespace App\Http\Controllers;
+class TallerController extends Controller{
+    public function index(){
+        echo "Hola Mundo";
+        return "Hola Mundo";
+    }
+}
